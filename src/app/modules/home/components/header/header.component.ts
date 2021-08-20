9f30cb7409caf1b9bea8f3a408ee610502a1326c
1und0 <mail@1und0.com>
2021-08-20 13:57:56 +0200

    integrate backnavigateion into navbar

--- src/app/modules/home/components/header/header.component.ts
+++ src/app/modules/home/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Router, NavigationEnd, Event as NavigationEvent } from '@angular/router';
 import { SearchValueService } from "src/app/core/services/searchvalue.service";
 
 @Component({
@@ -12,12 +12,28 @@ export class HeaderComponent {
 
   public searchvalue: string = '';
 
+  @ViewChild('backhome') test!: ElementRef;
 
   constructor(
     private router:Router,
-    private searchValueService: SearchValueService
-    ) {
+    private searchValueService: SearchValueService,
+    private element: ElementRef,
+    ) {}
 
+  ngAfterViewInit() {
+    this.router.events
+      .subscribe(
+        (event: NavigationEvent) => {
+          if(event instanceof NavigationEnd) {
+            console.log(this.test);
+            if (event.url!="/") {
+              this.test.nativeElement.firstChild.textContent='❮❮ Back';
+            }
+            else {
+              this.test.nativeElement.firstChild.textContent='wla-meets-lastfm';
+            }
+          }
+        });
   }
 
   onChange() {
